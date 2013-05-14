072ca5f4c2d7f82cfa365e129fe6361f539b2066
Yulai de Meer Fjeld <yulai.fjeld@gmail.com>
2013-05-14 19:34:56 +0200

    with help option

--- weather_test.py
+++ weather_test.py
@@ -1,5 +1,3 @@
-## GITHUB TEST
-
 ## import statements
 import os
 from os import *
@@ -26,35 +24,48 @@ london = 'BBCWeatherAlert'
 BBC_news = 'BBCBreaking'
 
 ## Query weather forecast
-user_input = raw_input("What can I do: ")
-if "weather" in user_input:
-    system('say Which city would you check?')
-    city = raw_input("Choose city: ")
-    if city == "Oslo":
-        weather_source = api.GetUserTimeline(oslo)
-    elif city == "London" :
-        weather_source = api.GetUserTimeline(london)    
-    else:
-        system('say Sorry, but I do not have that city.')
-        sys.exit(0)
-    system('say The weather forecast for ' + city + ' is')
-    read_weather = [s.text for s in weather_source]
-    weather_content = read_weather[0]
-    str(weather_content)
-    print(weather_content)
-    # replace parentheses with white spaces in python (applies to @weatherinoslo)
-    system('say '+ weather_content)
+def get_web_data():
+    user_input = raw_input("What can I do: ")
+    if "help" in user_input:
+        system('say Here is a list of available commands')
+        print ("\n**************")
+        print ("weather - check the weather forecast in a specific city")
+        print ("news - get the latest news from @BBCBreaking")
+        print ("**************\n")
+        get_web_data()
+    ## Weather
+    elif "weather" in user_input:
+        system('say Which city would you check?')
+        city = raw_input("Choose city: ")
+        if city == "Oslo":
+            weather_source = api.GetUserTimeline(oslo)
+        elif city == "London" :
+            weather_source = api.GetUserTimeline(london)    
+        else:
+            system('say Sorry, but I do not have that city.')
+            sys.exit(0)
+        system('say The weather forecast for ' + city + ' is')
+        read_weather = [s.text for s in weather_source]
+        weather_content = read_weather[0]
+        str(weather_content)
+        print(weather_content)
+        # replace parentheses with white spaces in python (applies to @weatherinoslo)
+        system('say '+ weather_content)
 
-## News
-elif "news" in user_input:
-    system('say I will check the latest news.')
-    news_source = api.GetUserTimeline(BBC_news)
-    read_news = [s.text for s in news_source]
-    news_content = read_news[0]
-    str(news_content)
-    print(news_content)
-    system('say Latest news is ' + news_content)
-## Default    
-else:
-    system('say I am sorry, but I do not understand your query.')
+    ## News
+    elif "news" in user_input:
+        if "please" in user_input:
+            system('say Thank you for asking politely.')
+        system('say I will check the latest news.')
+        news_source = api.GetUserTimeline(BBC_news)
+        read_news = [s.text for s in news_source]
+        news_content = read_news[0]
+        str(news_content)
+        print(news_content)
+        system('say Latest news is ' + news_content)
+    ## Default    
+    else:
+        system('say I am sorry, but I do not understand your query.')
 #endregion
+
+get_web_data()
