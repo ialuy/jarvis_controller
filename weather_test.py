677acf5e5a4a380fd2641b26687d2f248036462b
Yulai de Meer Fjeld <yulai.fjeld@gmail.com>
2013-05-15 01:06:43 +0200

    cleaning up

--- weather_test.py
+++ weather_test.py
@@ -4,6 +4,7 @@ from os import *
 import sys
 import twitter
 import time
+import re
 #endregion
 
 ## Authenticate yourself with Twitter
@@ -28,44 +29,43 @@ def get_web_data():
     user_input = raw_input("What can I do: ")
     if "help" in user_input:
         system('say Here is a list of available commands')
-        print ("\n**************")
-        print ("weather - check the weather forecast in a specific city")
-        print ("news - get the latest news from @BBCBreaking")
-        print ("**************\n")
+        print ("""\n**************
+weather - check the weather forecast in a specific city
+news - get the latest news from @BBCBreaking
+**************\n""")
         get_web_data()
+
     ## Weather
     elif "weather" in user_input:
         system('say Which city would you check?')
         city = raw_input("Choose city: ")
         if city == "Oslo":
-            weather_source = api.GetUserTimeline(oslo)
+            twitter_adr = oslo
         elif city == "London" :
-            weather_source = api.GetUserTimeline(london)    
+            twitter_adr = london
         else:
             system('say Sorry, but I do not have that city.')
             sys.exit(0)
         system('say The weather forecast for ' + city + ' is')
-        read_weather = [s.text for s in weather_source]
-        weather_content = read_weather[0]
-        str(weather_content)
-        print(weather_content)
-        # replace parentheses with white spaces in python (applies to @weatherinoslo)
-        system('say '+ weather_content)
 
     ## News
     elif "news" in user_input:
+        twitter_adr = BBC_news
         if "please" in user_input:
             system('say Thank you for asking politely.')
-        system('say I will check the latest news.')
-        news_source = api.GetUserTimeline(BBC_news)
-        read_news = [s.text for s in news_source]
-        news_content = read_news[0]
-        str(news_content)
-        print(news_content)
-        system('say Latest news is ' + news_content)
+        system('say The latest news from BBC are: ')
+
     ## Default    
     else:
         system('say I am sorry, but I do not understand your query.')
+        sys.exit(0)
+
+    get_data = api.GetUserTimeline(twitter_adr)
+    read_data = [s.text for s in get_data]
+    data_content = read_data[0]
+    print(str(data_content))    
+    mod_data = re.sub(r"[#&/]", "", data_content) # alter list of unwanted characters
+    system('say ' + mod_data)
 #endregion
 
 get_web_data()
